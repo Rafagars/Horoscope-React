244aced120973c44485f446b7bc38a2b6e6bf11a
rafagar96@gmail.com <rafagar96@gmail.com>
2020-06-26 14:43:51 -0400

    Updates

---
+++ precache-manifest.d98462c01c37c8de51704aa50b1fc725.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b5e9e5190ff55172b637ceb400e5b2e",
+    "url": "/Horoscope-React/index.html"
+  },
+  {
+    "revision": "56ee6c8a4571c58a6274",
+    "url": "/Horoscope-React/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "610462edfad679086249",
+    "url": "/Horoscope-React/static/css/main.fd59cc4f.chunk.css"
+  },
+  {
+    "revision": "56ee6c8a4571c58a6274",
+    "url": "/Horoscope-React/static/js/2.553e1773.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Horoscope-React/static/js/2.553e1773.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "610462edfad679086249",
+    "url": "/Horoscope-React/static/js/main.15caae59.chunk.js"
+  },
+  {
+    "revision": "f63555d5f50ba22d175e",
+    "url": "/Horoscope-React/static/js/runtime-main.b0c421bf.js"
+  }
+]);
